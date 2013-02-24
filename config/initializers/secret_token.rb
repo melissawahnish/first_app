15e851a2dfd48a15a4fb8fa1d773263680d95491
VMplanet Virtual Machines <vmplanet@ubuntu.(none)>
2013-02-24 09:29:00 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'eb0bb3664c715a081872e185c0cab8d7d3b59f99295da509e4eca6c3afb72b3ba986af3d63ef873b3e91372a6da568855a01658314e4446f9271a743662eed1f'
